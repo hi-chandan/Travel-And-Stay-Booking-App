46e74d07cac24b57bb2dab10715644d80d38c66e
LEO <hick317@gmail.com>
2024-06-14 09:24:01 +0530

    change login and registeration padding

--- src/app/(page)/login/page.jsx
+++ src/app/(page)/login/page.jsx
@@ -37,7 +37,7 @@ const Page = () => {
   };
 
   return (
-    <div className=" p-20 max-sm:flex-col  w-full flex sm:ml-20 justify-center items-center gap-10    ">
+    <div className=" p-6 max-sm:flex-col  w-full flex sm:ml-20 justify-center items-center gap-10    ">
       <div className="flex flex-col max-sm:w-full  w-1/4  rounded-md justify-center items-center p-4 bg-card text-copy-secondary  sm:mb-20  border  border-border  ">
         <h1 className="text-lg font-bold">Welcome Back </h1>
         <form
@@ -67,7 +67,7 @@ const Page = () => {
             href={"/registration"}
             className="text-green-700 pl-2 font-bold"
           >
-            Signup
+            Sign-up
           </Link>
         </span>
       </div>
